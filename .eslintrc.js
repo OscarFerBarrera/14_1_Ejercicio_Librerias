0c495c724812aedfd1dfeb34cee7c4b0ba7aeb13
OscarFerBarrera <osjamfer@hotmail.com>
2024-02-20 23:06:52 +0100

    Intentando comittar

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+    {
+      files: [
+        '.eslintrc.{js,cjs}',
+        '**/*.test.js',
+        '**/*.spec.js'
+      ],
+      env: {
+        node: true,
+        jest: true
+      },
+      parserOptions: {
+        sourceType: 'module'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    semi: 'off',
+    'react/react-in-jsx-scope': 'off',
+    'space-before-function-paren': 'off',
+    'react/jsx-indent': [2, 2],
+    'react/prop-types': 'off',
+    'jsx-quotes': [2, 'prefer-single']
+  }
+}
